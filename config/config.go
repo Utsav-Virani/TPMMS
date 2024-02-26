72911066818a55099d8a6c5e19e9c52b8720b72e
Utsav-Virani <uvvirani004@gmail.com>
2024-02-26 08:12:01 -0500

    Fiel saperation complete| Data added

---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+var NumLinesPerFile = 10
+var Inputfile1 = "r1_small.txt"
+var Inputfile2 = "r2_small.txt"
+var numberofFiles = 20
--- main.go
+++ main.go
@@ -1,7 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"TPMMS/config"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := recreateDirectory("firstpass")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Remove "secondpass" directory if it exists, and create a new one
+	err = recreateDirectory("secondpass")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileSplitter("firstpass/r1_", config.Inputfile1)
+	fileSplitter("firstpass/r2_", config.Inputfile2)
+}
+
+func recreateDirectory(dirName string) error {
+	// Check if directory exists
+	if _, err := os.Stat(dirName); err == nil {
+		// Directory exists, remove it
+		err := os.RemoveAll(dirName)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Create the directory
+	err := os.MkdirAll(dirName, 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func fileSplitter(OutputfilePrefix string, filename string) {
+	inputFileHandle, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inputFileHandle.Close()
+	scanner := bufio.NewScanner(inputFileHandle)
+	fileCount := 1
+	linesWritten := 0
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// defer outputFile.Close()
+	var outputFile *os.File
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			if linesWritten >= config.NumLinesPerFile {
+				if outputFile != nil {
+					outputFile.Close()
+				}
+				fileCount++
+				var err error
+				outputFile, err = createOutputFile(OutputfilePrefix, fileCount)
+				if err != nil {
+					log.Fatal(err)
+				}
+				linesWritten = 0
+			}
+			if linesWritten == 0 {
+				outputFile, err = createOutputFile(OutputfilePrefix, fileCount)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+			_, err := outputFile.WriteString(line + "\n")
+			if err != nil {
+				log.Fatal(err)
+			}
+			linesWritten++
+		} else {
+			break
+		}
+	}
+
+	if outputFile != nil {
+		outputFile.Close()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func createOutputFile(prefix string, count int) (*os.File, error) {
+	filename := fmt.Sprintf("%s_%d.txt", prefix, count)
+	return os.Create(filename)
 }
